5cc79de5e9cba20085978b595e0a8ddbad383257
williamzabot <williamzabot123@gmail.com>
2024-07-09 15:26:23 -0300

    remove unused configs

--- src/main/kotlin/com/williamzabot/clinicakotlin/ClinicaKotlinApplication.kt
+++ src/main/kotlin/com/williamzabot/clinicakotlin/ClinicaKotlinApplication.kt
@@ -2,8 +2,11 @@ package com.williamzabot.clinicakotlin
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = ["com.williamzabot.clinicakotlin.repositories"])
 class ClinicaKotlinApplication {
 
     companion object {
