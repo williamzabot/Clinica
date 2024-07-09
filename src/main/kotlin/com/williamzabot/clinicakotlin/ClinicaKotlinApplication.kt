41f080c32188baaab4f6f08dc34b4f36121ff56e
williamzabot <williamzabot123@gmail.com>
2024-07-09 19:43:07 -0300

    show all logs

--- src/main/kotlin/com/williamzabot/clinicakotlin/ClinicaKotlinApplication.kt
+++ src/main/kotlin/com/williamzabot/clinicakotlin/ClinicaKotlinApplication.kt
@@ -1,5 +1,7 @@
 package com.williamzabot.clinicakotlin
 
+import org.slf4j.LoggerFactory
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
@@ -10,10 +12,16 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 class ClinicaKotlinApplication {
 
     companion object {
+        val LOG = LoggerFactory.getLogger(ClinicaKotlinApplication::class.java)
         @JvmStatic
         fun main(args: Array<String>) {
+
             runApplication<ClinicaKotlinApplication>(*args)
+            SpringApplication.run(ClinicaKotlinApplication::class.java, *args).use {
+                context -> LOG.trace("context: $context")
+            }
         }
+
     }
 }
 
